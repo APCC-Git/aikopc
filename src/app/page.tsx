4424a14b8423ea18c8b43c88816d42a8a6025e86
6Rix6 <tesimariku0428@outlook.jp>
2025-05-09 15:59:44 +0900

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import SmoothScroll from '@/features/common/SmoothScroll';
 import Header from '@/components/HeaderOutline';
+import Footer from '@/components/Footer';
 import Window from '@/components/Window';
 import WindowSvg from '@/components/WindowSvgTest';
 
@@ -8,14 +9,11 @@ export default function Home() {
   const bg = 'black';
 
   return (
-    <div className={'min-h-screen'} style={{ backgroundColor: bg }}>
-      <div className={'w-full h-full fixed flex items-center z-0'}>
-        <div className={'w-full h-1/2 flex'}></div>
-      </div>
+    <div className={''} style={{ backgroundColor: bg }}>
       <Header rainbow={true} singleColor={'orange'} bg={bg} color={'white'} textColor={'white'} />
       <WindowSvg></WindowSvg>
       <WindowSvg></WindowSvg>
-      <div className={'h-20 w-full mt-20'}></div>
+      <Footer></Footer>
     </div>
   );
 }
