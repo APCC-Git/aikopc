a4267458a64d46279a0d6a9ce3ca01f122cccedc
6Rix6 <tesimariku0428@outlook.jp>
2025-05-13 00:55:44 +0900

    add title form

--- src/app/(user)/dashboard/blog/create/page.tsx
+++ src/app/(user)/dashboard/blog/create/page.tsx
@@ -1,9 +1,10 @@
 import { DashboardBreadcrumb } from '@/components/DashboardBreadcrumb';
 import { SimpleEditor } from '@/components/tiptap-templates/simple/simple-editor';
+import { Input } from '@/components/ui/input';
 
 export default function Page() {
   return (
-    <div className={'h-full flex flex-col'}>
+    <div className={'w-full flex flex-col overflow-scroll'}>
       <DashboardBreadcrumb
         items={[
           { label: 'ダッシュボード', href: '/dashboard' },
@@ -11,8 +12,11 @@ export default function Page() {
           { label: 'ブログ作成' },
         ]}
       />
-      <div className="w-full h-full max-h-full p-2 md:p-6">
-        <div className="shadow-sm w-full max-h-full">
+      <div className="w-full h-full max-h-full p-2 md:p-6 flex flex-col justify-between overflow-scroll">
+        <Input placeholder="タイトルを入力" className={"mt-10"}/>
+        <Input placeholder="タイトルを入力" className={"mt-10"}/>
+        <Input placeholder="タイトルを入力" className={"mt-10"}/>
+        <div className="shadow-sm w-full max-h-full mt-10">
           <SimpleEditor />
         </div>
       </div>
